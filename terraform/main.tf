618feb2ae5f1e96a26da0343c8573f688eca88f5
Paul Jickling <paul.jickling@ethereum.org>
2023-09-26 13:43:57 -0400

    init core_instance resource
    Signed-off-by: Paul Jickling <paul.jickling@ethereum.org>

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,23 @@ provider "oci" {
   auth = "OracleSecurityToken"
   config_file_profile = "default"
 }
+
+resource "oci_core_instance" "ubuntu_instance" {
+    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+    compartment_id = "<compartment-ocid>"
+    shape = "VM.Standard2.1"
+    source_details {
+      source_id = "<source-ocid>"
+      source_type = "image"
+    }
+
+    display_name = "foundry-vm"
+    create_vnic_details {
+      assign_public_ip = true
+      subnet_id = "<subnet-ocid>"
+    }
+    metadata = {
+      ssh_authorized_keys = file("<ssh-public-key-path>")
+    }
+    preserve_boot_volume = false
+}
